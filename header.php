0aed13dfbb99fb0a682b0b7c6d84ab2d13b6f39a
pjuros <petar.juros@gmail.com>
2016-12-13 22:23:01 +0100

    Relative paths

--- header.php
+++ header.php
@@ -5,21 +5,21 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>UIkit admin dashboard</title>
-	<link href="/uikit/css/uikit.almost-flat.min.css" rel="stylesheet">
-	<link href="/css/app.css" rel="stylesheet">
-	<script src="/js/jquery-3.1.0.min.js"></script>	
-	<script src="/uikit/js/uikit.min.js"></script>
+	<link href="uikit/css/uikit.almost-flat.min.css" rel="stylesheet">
+	<link href="css/app.css" rel="stylesheet">
+	<script src="js/jquery-3.1.0.min.js"></script>	
+	<script src="uikit/js/uikit.min.js"></script>
 </head>
 
 <body class="uk-height-1-1" style="padding-top: 42px;">
 
 	<nav class="uk-navbar uk-navbar-attached app-navbar">
-		<a href="/" class="uk-navbar-brand">UIkit admin dashboard</a>
+		<a href="index.php" class="uk-navbar-brand">UIkit admin dashboard</a>
 		<div class="uk-navbar-flip">
 			<ul class="uk-navbar-nav">
-				<li><a href="/"><i class="uk-icon-user"></i> Username</a></li>
-				<li><a href="/"><i class="uk-icon-cog uk-icon-justify"></i> Settings</a></li>
-				<li><a href="/"><i class="uk-icon-power-off uk-icon-justify"></i> Log out</a></li>
+				<li><a href="index.php"><i class="uk-icon-user"></i> Username</a></li>
+				<li><a href="index.php"><i class="uk-icon-cog uk-icon-justify"></i> Settings</a></li>
+				<li><a href="index.php"><i class="uk-icon-power-off uk-icon-justify"></i> Log out</a></li>
 			</ul>
 		</div>
 	</nav>
@@ -34,7 +34,7 @@
 			</div>
 			<ul data-uk-nav="" class="uk-nav uk-nav-side uk-nav-parent-icon">
 				<li class="uk-nav-divider"></li>
-				<li><a href="/"><i class="uk-icon-dashboard uk-icon-justify"></i> Dashboard</a></li>
+				<li><a href="index.php"><i class="uk-icon-dashboard uk-icon-justify"></i> Dashboard</a></li>
 				<li><a href="tables.php"><i class="uk-icon-table uk-icon-justify"></i> Tables</a></li>
 				<li><a href="panels.php"><i class="uk-icon-th-large uk-icon-justify"></i> Panels</a></li>
 				<li><a href="buttons.php"><i class="uk-icon-square-o uk-icon-justify"></i> Buttons</a></li>
